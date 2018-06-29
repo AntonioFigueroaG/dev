c92b2d0d9769dab56a69237ba205626384f316be
nidiag <40703589+nidiag@users.noreply.github.com>
2018-06-29 15:55:47 -0600

    prueba de prueba.php
    verificar prueba

--- Production/Kiosko/prueba.php
+++ Production/Kiosko/prueba.php
@@ -6,55 +6,30 @@
 		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 </head>
 <style>
-	form{ 
-	margin: 0 auto;
-    width: 400px;
-	padding: 1em;
-	border: 1px solid #CCC;
-	border-radius: 1em;
-	}
+	form{margin: 0 auto; width: 400px; padding: 1em; border: 1px solid #CCC; border-radius: 1em; background-color:whitesmoke;}
 	
-	form div + div {
-    margin-top: 1em;
-	}	
-	label {
-    display: inline-block;
-    width: 90px;
-    text-align: right;
-	}
-	input, textarea {
-    font: 1em sans-serif;
-
-    width: 300px;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-
-    border: 1px solid #999;
-	}
-	input:focus, textarea:focus {
-    border-color: #000;
-	}
-	textarea {
-    vertical-align: top;
-		
-    height: 5em;
-
-    resize: vertical;
-	}
-	h1{
-		background-color:darkslateblue;
-		text-align: center;
-		color: white;
-	}
+	form div + div { margin-top: 1em;}	
+	label {display: inline-block; width: 90px; text-align: right;}
+	
+	input, textarea {font: 1em sans-serif; width: 300px; -moz-box-sizing: border-box; box-sizing: border-box; border: 1px solid #999;}
+	
+	input:focus, textarea:focus { border-color: #000;}
+	
+	textarea {vertical-align: top; height: 5em; resize: vertical;}
+	
+	img {padding-top: 5px;}
+	h1 {background-color: #3f51b5;margin: 0px;height: auto;align-content: center;}
 	
 </style>
-<body>
+<body style="margin: 0px" bgcolor="#C6C6C6">
+		<div id="cabecera"><h1><center><img style="width: 310px" src="https://loadtrail.com/wp-content/uploads/2018/01/Webiste_LT-New-Logo_2018_FINAL-2.png" alt="Load Trail LLC" title="Load Trail LLC"></center></h1>
+		<center><h2>Create vin</h2></center></div>
+		</br>
 	<center>
-		<h1>Load Trail</h1>
 		<form>
 			<div>
 				<label for="vin">Vin:</label>
-				<input type="text" id="vin" autofocus />
+				<input type="number" id="vin" autofocus min="1000000" title="Add a vin"/> 
 			</div>
 			<div>
 				<label for="description">Description:</label>
@@ -70,31 +45,41 @@
 	function save(){
 		var nvin = document.getElementById("vin").value;
 		var des = document.getElementById("description").value;
-		if(nvin==""){
-			alert("Please fill vin");
+		if(nvin.length<6){
+			alert("Invalid vin");
 			document.getElementById("vin").focus();
 		}else{
-			if(des==""){
-				alert("Please add description");
-				document.getElementById("description").focus();
+			if(nvin==""){
+				alert("Please fill vin");
+				document.getElementById("vin").focus();
 			}else{
-				var parametros={
-				"nvin": nvin,
-				"des": des,
-			};
-				$.ajax({
-					data: parametros,
-					url:   'kioskoMSC/loquesea.php',
-					type:  'post',
-					beforeSend: function () {
-					},
-					success:  function (response) {	
-						alert(response);
-					}
-				});
+				if(des==""){
+					alert("Please add description");
+					document.getElementById("description").focus();
+				}else{
+					var parametros={
+					"nvin": nvin,
+					"des": des,
+				};
+					$.ajax({
+						data: parametros,
+						url:   'kioskoMSC/loquesea.php',
+						type:  'post',
+						beforeSend: function () {
+						},
+						success:  function (response) {	
+							if(response=="Existing vin"){
+								alert(response);
+								document.getElementById("vin").focus();
+								document.getElementById("vin").value="";
+							}else{
+								window.location.reload();
+							}
+						}
+					});
+				}
 			}
 		}
 	}
-	
 </script>
 </html>
